1a1133f32c8c376588a55dce1c7a8852c60ba6d8
bjwswang <bjwswang@gmail.com>
2023-12-06 10:39:41 +0800

    init commit for image sync
    Signed-off-by: bjwswang <bjwswang@gmail.com>

---
+++ sync.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Read JSON data from file
+json_data=$(cat images.json)
+
+# Docker Hub username
+username="kubeagi"
+
+
+# Log in to Docker Hub
+echo "$DOCKER_TOKEN" | docker login -u "$username" --password-stdin
+
+# Iterate over each image mapping
+echo "$json_data" | jq -c '.[]' | while IFS= read -r mapping; do
+  from_image=$(echo "$mapping" | jq -r '.from')
+  to_image=$(echo "$mapping" | jq -r '.to')
+
+  echo "sync image from $from_image to $to_image"
+
+  # Pull the "from" image
+  docker pull "$from_image"
+
+  # Tag the "from" image with the "to" image name
+  docker tag "$from_image" "$to_image"
+
+  # Push the "to" image to Docker Hub
+  docker push "$to_image"
+done
